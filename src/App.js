b3cfc6069a699040dcbd50742376c9ea8c85e6df
Code Explained <codeexplainedorg@gmail.com>
2024-06-25 17:06:47 +0100

    Starter template

---
+++ src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className='App'>
+      <h1>Happy Coding!</h1>
+      <h2>ReactJs Weather App | By Code Explained</h2>
+    </div>
+  );
+}
+
+export default App;
